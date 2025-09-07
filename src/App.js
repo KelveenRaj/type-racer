946676f0d9fe2db1374e2c9258c3c394d17247c8
Kelveen Raj <kelveenrajv@gmail.com>
2025-09-07 12:55:04 +0800

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TypingGame from "./components/TypingGame";
+
+const App = () => {
+  return (
+    <>
+      <TypingGame />
+    </>
+  );
+};
+
+export default App;
